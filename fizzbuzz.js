86de6393733ef783d5c5efa78c46a75db7a763b8
iPenguin67 <ipenguin@gmail.com>
2018-04-04 21:51:13 -0500

    Add files via upload

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,9 @@
+
 for (i=1; i<101; i++) {
+  console.log(i % 3 === 0 && i % 5 === 0 ? "FizzBuzz" : i % 5 === 0 ? "Buzz" : i % 3 === 0 ? "Fizz" : i)
+}
+
+/*for (i=1; i<101; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
     console.log("FizzBuzz");
   }
@@ -12,3 +17,5 @@ for (i=1; i<101; i++) {
     console.log(i)
   }
 }
+
+*/
